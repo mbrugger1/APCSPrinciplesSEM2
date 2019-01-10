0b4a9baf444ed9901865ff175d81530bd89a1db9
mbrugger1 <43447435+mbrugger1@users.noreply.github.com>
2019-01-10 08:49:17 -0800

    bub

--- Labs/Lab 109/bubblysort.js
+++ Labs/Lab 109/bubblysort.js
@@ -1,1 +1,21 @@
 //Markus Brugger
+function bubbleSort(txt) {
+
+var length = txt.length;
+for (var i = 0; i < length, i++){
+
+  for (var j = 0; j <(length - i - 1); j++){
+
+  if(txt[j] > txt[j+1]);
+   {
+    var temp = txt[j];
+
+    txt[j] = txt[j+1];
+
+    txt[j+1] = temp;
+
+    console.log(txt);
+  }
+}
+}
+}
