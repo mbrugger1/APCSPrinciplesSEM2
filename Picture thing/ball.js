4e066721130e475a9f6a708065f4b44d3d0ba1d8
mbrugger1 <43447435+mbrugger1@users.noreply.github.com>
2019-02-28 08:33:33 -0800

    picttarea

--- Picture thing/ball.js
+++ Picture thing/ball.js
@@ -1,42 +1,50 @@
-/*
-**  Ball Constructor Function
-**  markus
-**  Aug 20, 2018
-*/
-function Ball(location, velocity, radius, col){
+function Ball(loc, vel, rad, col){
   // Instance variables
-   this.loc = location;
-   this.vel = velocity;    //?
-   this.rad = radius;
-   this.col = col;
-   this.acc = createVector(0,1);
- this.run = function(){
-     this.checkEdges();
-     this.update();
-     this.render();
- }
-
-   this.update = function(){
-    var d = this.loc.dist(ball.loc)
-    var attForce = p5.Vector.sub(ball.loc, this.loc);
-    attForce.normalize();
-    attForce.mult(.02);
-    this.vel.limit(3);
+  this.loc = loc;
+  this.vel = vel;
+  this.rad = rad;
+  this.col = col;
+  //this function calls other functions
+  this.run = function(){
+    this.checkEdges();
+    this.update();
+    this.render();
+  }
+  //This function changes the location of the ball
+  //by adding speed to x and y
+  this.update = function(){
+    if(this !== b1){
+      var d = this.loc.dist(b1.loc);
+      if(d>250){
+        var attForce = p5.Vector.sub(b1.loc, this.loc);
+        attForce.normalize();
+        attForce.mult(1);
+        this.vel.add(attForce);
+      }
+      if(d<85){
+        var repForce = p5.Vector.add(b1.loc, this.loc);
+        repForce.normalize();
+        repForce.mult(.05);
+        this.vel.add(repForce);
+      }
+    }
     this.loc.add(this.vel);
   }
+  //checkEdges() reverses speed when the ball touches an edge
+  this.checkEdges = function(){
+    if(this.loc.x < 0) this.vel.x = -this.vel.x;
+    if(this.loc.x > width) this.vel.x = -this.vel.x;
+    if(this.loc.y < 0) this.vel.y = -this.vel.y;
+    if(this.loc.y > height) this.vel.y = -this.vel.y;
+  }
 
-    }
-   this.checkEdges = function(){
-      if(this.loc.x < 0) this.vel.x = -this.vel.x;
-      if(this.loc.x > width) this.vel.x = -this.vel.x;
-      if(this.loc.y < 0) this.vel.y = -this.vel.y;
-      if(this.loc.y > height) this.vel.y = -this.vel.y;
-   }
-this.render = function(){
-//  fill(this.col);
-  stroke(255,150);
-  strokeWeight(1.5);
-  line(this.loc.x, this.loc.y);
+  //render() draws the ball at the new location
+  this.render = function(){
+    stroke(random(0, 255), random(0, 255), random(200, 255));
+    strokeWeight(2)
+    line(this.loc.x, this.loc.y, 800, 800);
+    line(this.loc.x, this.loc.y, 0, 0);
+    line(this.loc.x, this.loc.y, 800, 0);
+    line(this.loc.x, this.loc.y, 0, 800);
+  }
 }
-
-//comment
--- Picture thing/sketch.js
+++ Picture thing/sketch.js
@@ -1,32 +1,30 @@
-//  Global variables
-var balls = [];
-var ball;
-//seting up the function
-function setup() {
+var Balls = [];
+var b1;
+
+function setup(){
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(random(20,250),random(20,250),random(20,250), 25);
-  loadBalls(2000);
-  ball = new Ball(createVector(width/2, height/2));
+  background(20, 20, 20);
+  loadBalls(1);
+  b1 = new Ball(createVector(width/2, height/2), createVector(2,5), 100, color(255, 0, 0));
 }
-//not actually drawing anything here
-function draw() {
- background(random(20,50),random(20,250),random(20,250), 22);
- ball.run();
-   for(var i = 0; i < balls.length; i = i + 1){
-    balls[i].run();
 
-}
-//loading the balls
-function loadBalls(){
-
-          for(var i = 0; i < 200; i++){
-          var loc = createVector(random(width), random(height));
-          var vel = createVector(random(-3,3), random(-3,3));
-          var radius = random(2,5);
-          var col = color(random(255), random(255), random(255));
-          var b = new Ball(loc, vel, radius, col);
-          balls.push(b);
+function loadBalls(number){
+  for(var i = 0; i < number; i = i + 1){
+    var loc = createVector(random(width), random(height));
+    var vel = createVector(random(-3,3), random(-3,3));
+    var rad = random(20, 40);
+    var col = color(random(255), random(255), random(255));
+    Balls.push(new Ball(loc, vel, rad, col));
   }
+}
+
+
+
+function draw(){
+  background(random(0, 255), random(0, 255), random(0, 255), 5);
+  b1.run();
+  for(var i = 0; i < Balls.length; i = i + 1){
+    Balls[i].run();
   }
 }
